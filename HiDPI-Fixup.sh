95567c6d4c9c864f10bc5462cb22ea7c4bb0ab61
Mykola Grymalyuk <48863253+khronokernel@users.noreply.github.com>
2020-08-07 09:50:29 -0600

    Add prompt for reboot

--- HiDPI-Fixup.sh
+++ HiDPI-Fixup.sh
@@ -4,7 +4,13 @@ echo -e "\033[0;31m Starting HiDPI script \033[0m"
 
 sudo -v
 
+## Allow custom HiDPI resolution
+echo -e "\033[0;32m Patching windowserver preferences \033[0m"
+defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool YES
+defaults delete /Library/Preferences/com.apple.windowserver DisplayResolutionDisabled
+
 ## Create folders
+echo -e "\033[0;32m Creating Display Override folders \033[0m"
 mkdir Displays
 cd /Displays
 mkdir Contents
@@ -17,5 +23,14 @@ mkdir DisplayVendorID-30e4
 cd
 
 ## Copy over HiDPI plist
-
+echo -e "\033[0;32m Moving HiDPI plist to correct location \033[0m"
 cp /System-Fixup/Display-Fixup/DisplayProductID-4a5.plist /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/DisplayProductID-4a5
+
+## Reboot
+
+echo "Finished running the script, would you loke to reboot?"
+select yn in "Yes" "No"
+case $yn in
+    Yes ) shutdown -r now;;
+    No ) exit;;
+esac
