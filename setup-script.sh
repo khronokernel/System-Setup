b17503ec34d48354ae1e9b3ab235f9dbf01e1c60
Mykola Grymalyuk <48863253+khronokernel@users.noreply.github.com>
2020-08-11 17:01:12 -0600

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -29,7 +29,17 @@ PACKAGES=(
 )
 
 echo -e "\033[0;31m Installing packages...\033[0m"
-brew install ${PACKAGES[@]}
+#brew install ${PACKAGES[@]} Removing due to issues if one app failed
+
+brew install java
+brew install composer
+brew install python3
+brew install python
+brew install bash
+brew install yarn
+brew install node
+brew install wget
+brew install jq
 
 echo -e "\033[0;32m Cleaning up...\033[0m"
 brew cleanup
@@ -67,13 +77,41 @@ CASKS=(
 )
 
 echo -e "\033[0;32m Installing cask apps...\033[0m"
-brew cask install ${CASKS[@]}
+#brew cask install ${CASKS[@]} Removing due to issues if one app failed
+
+brew cask install beyond-compare
+brew cask install balenaetcher
+brew cask install adoptopenjdk
+brew cask install coolterm
+brew cask install discord
+brew cask install element
+brew cask install firefox
+brew cask install ghidra
+brew cask install google
+brew cask install github
+brew cask install -chrome
+brew cask install hex-fiend
+brew cask install idafree
+brew cask install istat-menus
+brew cask install keka
+brew cask install logitech-options
+brew cask install maciasl
+brew cask install minecraft
+brew cask install monitorcontrol
+brew cask install qbittorrent
+brew cask install steam
+brew cask install textmate
+brew cask install typora
+brew cask install vanilla
+brew cask install virtualbox
+brew cask install visual-studio-code
+
 
 echo -e "\033[0;31m Installing MAS...\033[0m"
 brew install mas
 
 MASS=(
-	#808809998 # Xcode
+	808809998 # Xcode
 	441258766 # Magnet
 	409201541 # Pages
 	409183694 # Keynote
@@ -83,7 +121,16 @@ MASS=(
 )
 	
 echo -e "\033[0;32m Installing MAS apps...\033[0m"
-mas install ${MASS[@]}
+#mas install ${MASS[@]} Removing due to issues if one app failed
+
+mas install 808809998
+mas install 441258766
+mas install 409201541
+mas install 409183694
+mas install 409203825
+mas install 408981434
+mas install 425424353
+
 
 echo -e "\033[0;31m Installing other macOS apps...\033[0m"
 
@@ -138,10 +185,14 @@ cd
 
 ### Due to issues with Big Sur and Python, I've added a few paths
 
-base_ver=11.0
-ver=$(sw_vers | grep ProductVersion | cut -d':' -f2 | tr -d ' ')
-if [ $(echo -e $base_ver"\n"$ver | sort -V | tail -1) == "$base_ver" ]
-then 
+KERNEL=$(uname -r)
+if [ $KERNEL > 19.9.9 ];then
+	echo -e "\033[0;32m Detected 11.0 or newer OS, Installing pre-built ProperTree \033[0m"
+	cd ~/System-Setup/ProperTree-GOOD
+	unzip ProperTree-11.zip
+	mv ProperTree.app /Applications/ProperTree.app
+	cd
+else
 	echo -e "\033[0;32m Detected 10.15 or older OS, Installing ProperTree from source\033[0m"
 	git clone https://github.com/corpnewt/ProperTree
 	cd ProperTree
@@ -149,12 +200,6 @@ then
 	cd Scripts
 	python3 buildapp.command
 	cd ..
-	mv ProperTree.app /Applications/ProperTree.app
-	cd
-else 
-	echo -e "\033[0;32m Detected 11.0 or newer OS, Installing pre-built ProperTree \033[0m"
-	cd ProperTree-GOOD
-	unzip ProperTree-11.zip
 	mv ProperTree.app /Applications/ProperTree.app
 	cd
 fi
